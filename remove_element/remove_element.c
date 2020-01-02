e0a8f71f911f5b7646d501c69e6086ebee5f6451
chenyulee <32009591+chenyulee@users.noreply.github.com>
2020-01-02 22:08:04 +0800

    更改27題
    沒有用而額外的記憶體

--- remove_element/remove_element.c
+++ remove_element/remove_element.c
@@ -1,26 +1,12 @@
-
-
-int removeElement(int* nums, int numsSize, int val) {
-	// [2,2,1,4,5,3,4,5] val=3
-	int answer[numsSize] = {};
-	int cnt = 0;
-	int i = 0;
-	for (i = 0; i < numsSize; i++) { // 找不是val的放到answer裡
-		// i == 7, nums[7] == 5, val == 3
-		if (nums[i] != val) {
-			// cnt = 5, 
-			answer[cnt] = nums[i];
-			cnt++; 
-		}
-	}
-	// [2,2,1,4,5,0,0,0]
-	
-
-	//把答案複製回去
-	for (int i = 0; i < cnt; i++) {
-		nums[i] = answer[i];
-	}
-
-	return cnt;
-
+int removeElement(int* nums, int numsSize, int val){
+    int i;
+    int j = 0;
+    for(i = 0; i < numsSize; i++){
+        if(nums[i] != val) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
 }
+
